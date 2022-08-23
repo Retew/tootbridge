e940beef520deea7a28809bbfc200420619c55a6
Retew <@TootBridge@mastodon.social>
2022-08-23 20:18:40 +0300

    Implemented signal handling and proper shutdown in docker

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 import asyncio
+import signal
+from functools import partial
 from itertools import count
 from os import getenv
 from pathlib import Path
+from typing import Any
 
 from loguru import logger
 
@@ -22,10 +25,25 @@ async def main() -> None:
         logger.debug(f"Run {i} started")
         try:
             await bridge_director.sync_bridges()
-        finally:
             bridge_director.update_credentials()
-        await asyncio.sleep(sleep_duration)
+            await asyncio.sleep(sleep_duration)
+        except asyncio.CancelledError:
+            logger.warning("Shutting down now")
+            bridge_director.update_credentials()
+            break
+
+
+def cancel_coroutine(target_coroutine: Any):
+    """A signal handler that cancels running tasks for the target coroutine"""
+    for task in asyncio.tasks.all_tasks():
+        if task.get_coro() is target_coroutine and not task.cancelled():
+            task.cancel()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main_task = main()
+    cancel_main_task = partial(cancel_coroutine, target_coroutine=main_task)
+    loop = asyncio.new_event_loop()
+    loop.add_signal_handler(signal.SIGINT, cancel_main_task)
+    loop.add_signal_handler(signal.SIGTERM, cancel_main_task)
+    loop.run_until_complete(main_task)
